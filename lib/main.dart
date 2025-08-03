a66d0f845dfbf4f7cb616464b125a23ee2403dea
Arturo Manuel Noriega Noriega <90386913+ArturoManuel@users.noreply.github.com>
2025-08-03 14:02:39 -0500

    splash-screen time

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,16 @@ import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
 import 'package:weatherapp/app/router/app_pages.dart';
-
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'app/bindings/initial_binding.dart';
 import 'app/ui/theme/app_theme.dart';
 
 void main() {
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
+   Future.delayed(const Duration(seconds: 3), () {
+    FlutterNativeSplash.remove();
+  });
   runApp(WeatherApp());
 }
 
